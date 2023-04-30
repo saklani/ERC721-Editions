53e46aeb08ef29dcee7475eea42d9768d95d9a87
saklani <50768838+saklani@users.noreply.github.com>
2023-04-30 23:00:00 +0530

    editions

--- src/Edition.sol
+++ src/Edition.sol
@@ -3,24 +3,25 @@ pragma solidity >=0.8.0;
 
 import "./ERC721.sol";
 
-contract ERC721Edition is ERC721 {
+contract Edition is ERC721 {
     /*----------------------------------------------------------------*
      |                             EVENTS                             |
      *----------------------------------------------------------------*/
 
     event PriceChange(uint256 amount);
-    event LimitChange(uint256 amount);
     event Mint(address indexed to, uint256 indexed tokenId);
 
     /*----------------------------------------------------------------*
      |                            METADATA                            |
      *----------------------------------------------------------------*/
+    // the current mint price for this edition
     uint256 private _mintPrice;
 
     function mintPrice() public view returns (uint256) {
         return _mintPrice;
     }
 
+    // the current mint limit for this edition
     uint256 private _mintLimit;
 
     function mintLimit() public view returns (uint256) {
@@ -43,15 +44,14 @@ contract ERC721Edition is ERC721 {
         _mintLimit = mintLimit_;
     }
 
-    /// @notice Mints a new NFT with `tokenId` for the address `to`. It makes sure that when the receiving address
-    ///  is a smart contract it doesn't lock the NFT.
-    /// @dev External function to mint tokens, also increments `tokenId`. This function should be used when minting.
-    /// @param to The minting address
-
     /*----------------------------------------------------------------*
      |                          MINT LOGIC                            |
      *----------------------------------------------------------------*/
 
+    /// @notice Mints a new NFT with `tokenId` for the address `to`. It makes sure that when the receiving address
+    ///  is a smart contract it doesn't lock the NFT.
+    /// @dev External function to mint tokens, also increments `tokenId`. This function should be used when minting.
+    /// @param to The minting address
     function mint(address to) external payable {
         if (msg.value < _mintPrice) {
             revert NOT_ENOUGH_ETH();
@@ -73,12 +73,5 @@ contract ERC721Edition is ERC721 {
     function updateMintPrice(uint256 newPrice) external onlyOwner {
         _mintPrice = newPrice;
         emit PriceChange(newPrice);
-    }
-
-    /// @notice Update mint limit of the contract
-    /// @dev Lets the owner of the contract update the mint price.
-    function updateMintLimit(uint256 newLimit) external onlyOwner {
-        _mintLimit = newLimit;
-        emit LimitChange(newLimit);
     }
 }
